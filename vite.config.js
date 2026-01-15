749af19c526fd0d61dee24f6cd30566ad59dfdb8
Ade Naufal Rianto <adenaufalr@gmail.com>
2026-01-15 17:56:26 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/ade-portfolio/",
+  publicDir: "public", // Memastikan semua file di public/ ter-copy ke dist/
 });
